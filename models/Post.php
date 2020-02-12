ec112a84a09691ff86bd32896bea3fe6898dce6b
SashaPlavi <sasha.bingulac@gmail.com>
2020-02-12 10:35:29 +0100

    create

--- models/Post.php
+++ models/Post.php
@@ -57,4 +57,34 @@ class Post
 
     return $stmt;
   }
+
+  public function create()
+  {
+    $query = 'INSERT INTO ' . $this->table . '
+    SET
+    title=:title,
+    body=:body,
+    author= :author,
+    category_id= :category_id
+    ';
+
+    $stmt = $this->connection->prepare($query);
+    //clean
+    $this->title = htmlspecialchars(strip_tags($this->title));
+    $this->body = htmlspecialchars(strip_tags($this->body));
+    $this->author = htmlspecialchars(strip_tags($this->author));
+    $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+    $stmt->bindParam(':title', $this->title);
+    $stmt->bindParam(':body', $this->body);
+    $stmt->bindParam(':author', $this->author);
+    $stmt->bindParam(':category_id', $this->category_id);
+
+    if ($stmt->execute()) {
+      return true;
+    } else {
+      printf("Error: %s.\n", $stmt->error);
+      return false;
+    }
+  }
 }
